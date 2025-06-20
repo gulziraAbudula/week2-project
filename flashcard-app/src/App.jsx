5ced4bc8a7099bfebac6e533ac087120da8334d8
Gulzira Abdullah <gulziraabdullah@mac.mynetworksettings.com>
2025-06-20 00:33:51 -0400

    adding gif

--- flashcard-app/src/App.jsx
+++ flashcard-app/src/App.jsx
@@ -14,6 +14,8 @@ function App() {
   return (
     <div className="app">
       <h1>Flashcard Study App</h1>
+      <h2>Let's study the capital of countries together!</h2>
+      <h3>total number of card: 3</h3>
       <FlashcardList cards={cards} />
     </div>
   );
